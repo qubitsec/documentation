d930daf5ac5697640b85ff20f898557710d31afa
Qubit Security Inc <plura@qubitsec.com>
2023-12-22 08:06:30 +0900

    Create start_plura_solr.sh

---
+++ rocky8/kvm/script/start_plura_solr.sh
@@ -0,0 +1,153 @@
+#!/usr/bin/env bash
+
+#mysql
+#virsh start 010240-mysql                   
+#virsh start 010241-mysql                   
+#virsh start 010242-mysql                   
+#virsh start 010243-mysql                   
+#virsh start 010244-mysql                   
+#virsh start 010245-mysql
+#virsh start 010246-mysql
+
+#virsh start 010251-solr-nfs
+
+#virsh start 010252-nfs
+
+virsh start 011020-solr
+virsh start 011021-solr
+
+virsh start 011022-solr
+virsh start 011023-solr
+
+virsh start 011024-solr
+virsh start 011025-solr
+
+virsh start 011026-solr
+virsh start 011027-solr
+
+virsh start 011028-solr
+virsh start 011029-solr
+
+virsh start 011030-solr
+virsh start 011031-solr
+
+virsh start 011032-solr
+virsh start 011033-solr
+
+virsh start 011034-solr
+virsh start 011035-solr
+
+virsh start 011036-solr
+virsh start 011037-solr
+
+virsh start 011038-solr
+virsh start 011039-solr
+
+virsh start 011040-solr
+virsh start 011041-solr
+
+virsh start 011042-solr
+virsh start 011043-solr
+
+virsh start 011044-solr
+virsh start 011045-solr
+
+virsh start 011046-solr
+virsh start 011047-solr
+
+virsh start 011048-solr
+virsh start 011049-solr
+
+virsh start 011050-solr
+virsh start 011051-solr
+
+virsh start 011052-solr
+virsh start 011053-solr
+
+virsh start 011060-solr
+virsh start 011061-solr
+
+virsh start 011062-solr
+virsh start 011063-solr
+
+virsh start 011064-solr
+virsh start 011065-solr
+
+virsh start 011066-solr
+virsh start 011067-solr
+
+virsh start 011068-solr
+virsh start 011069-solr
+
+virsh start 011070-solr
+virsh start 011071-solr
+
+virsh start 011100-solr
+virsh start 011101-solr
+
+virsh start 011102-solr
+virsh start 011103-solr
+
+virsh start 011104-solr
+virsh start 011105-solr
+
+virsh start 011106-solr
+virsh start 011107-solr
+
+virsh start 011108-solr
+virsh start 011109-solr
+
+
+virsh start 011120-solr
+virsh start 011121-solr
+
+virsh start 011122-solr
+virsh start 011123-solr
+
+virsh start 011130-solr
+virsh start 011131-solr
+
+virsh start 011132-solr
+virsh start 011133-solr
+
+virsh start 011140-solr
+virsh start 011141-solr
+
+virsh start 011142-solr
+virsh start 011143-solr
+
+virsh start 011160-solr
+virsh start 011161-solr
+
+virsh start 011162-solr
+virsh start 011163-solr
+
+virsh start 011170-solr
+virsh start 011171-solr
+
+virsh start 011172-solr
+virsh start 011173-solr
+
+virsh start 011180-solr
+virsh start 011181-solr
+
+virsh start 011182-solr
+virsh start 011183-solr
+
+virsh start 011184-solr
+virsh start 011185-solr
+
+virsh start 011190-solr
+virsh start 011191-solr
+
+virsh start 011192-solr
+virsh start 011193-solr
+
+virsh start 011194-solr
+virsh start 011195-solr
+
+virsh start 011200-solr-r
+virsh start 011201-solr-r
+
+virsh start 011202-solr-r
+virsh start 011203-solr-r
