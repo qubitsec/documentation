0eb7b92765181020efabc2765454d171a7971d07
Qubit Security Inc <plura@qubitsec.com>
2023-12-26 10:51:53 +0900

    Update start_plura_solr.sh

--- rocky8/kvm/script/start_plura_solr.sh
+++ rocky8/kvm/script/start_plura_solr.sh
@@ -1,43 +1,43 @@
 #!/usr/bin/env bash
 
 #mysql
-#virsh start 010240-mysql                   
-#virsh start 010241-mysql                   
-#virsh start 010242-mysql                   
-#virsh start 010243-mysql                   
-#virsh start 010244-mysql                   
-#virsh start 010245-mysql
-#virsh start 010246-mysql
+virsh start 010240-mysql                   
+virsh start 010241-mysql                   
+virsh start 010242-mysql                   
+virsh start 010243-mysql                   
+virsh start 010244-mysql                   
+virsh start 010245-mysql
+virsh start 010246-mysql
 
-virsh start 011020-solr
-virsh start 011021-solr
+virsh start 011020-solr-sys
+virsh start 011021-solr-sys
 
-virsh start 011022-solr
-virsh start 011023-solr
+virsh start 011022-solr-sys
+virsh start 011023-solr-sys
 
-virsh start 011024-solr
-virsh start 011025-solr
+virsh start 011024-solr-sys
+virsh start 011025-solr-sys
 
-virsh start 011026-solr
-virsh start 011027-solr
+virsh start 011026-solr-sys
+virsh start 011027-solr-sys
 
-virsh start 011028-solr
-virsh start 011029-solr
+virsh start 011028-solr-sys
+virsh start 011029-solr-sys
 
-virsh start 011030-solr
-virsh start 011031-solr
+virsh start 011030-solr-sys
+virsh start 011031-solr-sys
 
-virsh start 011032-solr
-virsh start 011033-solr
+virsh start 011032-solr-sys
+virsh start 011033-solr-sys
 
-virsh start 011034-solr
-virsh start 011035-solr
+virsh start 011034-solr-sys
+virsh start 011035-solr-sys
 
-virsh start 011036-solr
-virsh start 011037-solr
+virsh start 011036-solr-sys
+virsh start 011037-solr-sys
 
-virsh start 011038-solr
-virsh start 011039-solr
+virsh start 011038-solr-sys
+virsh start 011039-solr-sys
 
 virsh start 011040-solr
 virsh start 011041-solr
@@ -60,66 +60,66 @@ virsh start 011051-solr
 virsh start 011052-solr
 virsh start 011053-solr
 
-virsh start 011060-solr
-virsh start 011061-solr
+virsh start 011060-solr-net
+virsh start 011061-solr-net
 
-virsh start 011062-solr
-virsh start 011063-solr
+virsh start 011062-solr-net
+virsh start 011063-solr-net
 
-virsh start 011064-solr
-virsh start 011065-solr
+virsh start 011064-solr-net
+virsh start 011065-solr-net
 
-virsh start 011066-solr
-virsh start 011067-solr
+virsh start 011066-solr-net
+virsh start 011067-solr-net
 
-virsh start 011068-solr
-virsh start 011069-solr
+virsh start 011068-solr-net
+virsh start 011069-solr-net
 
 virsh start 011070-solr
 virsh start 011071-solr
 
-virsh start 011100-solr
-virsh start 011101-solr
+virsh start 011100-solr-etc
+virsh start 011101-solr-etc
 
-virsh start 011102-solr
-virsh start 011103-solr
+virsh start 011102-solr-etc
+virsh start 011103-solr-etc
 
-virsh start 011104-solr
-virsh start 011105-solr
+virsh start 011104-solr-etc
+virsh start 011105-solr-etc
 
-virsh start 011106-solr
-virsh start 011107-solr
+virsh start 011106-solr-etc
+virsh start 011107-solr-etc
 
-virsh start 011108-solr
-virsh start 011109-solr
+virsh start 011108-solr-etc
+virsh start 011109-solr-etc
 
 
-virsh start 011120-solr
-virsh start 011121-solr
+virsh start 011120-solr-sysd
+virsh start 011121-solr-sysd
 
-virsh start 011122-solr
-virsh start 011123-solr
+virsh start 011122-solr-sysd
+virsh start 011123-solr-sysd
 
-virsh start 011130-solr
-virsh start 011131-solr
+virsh start 011130-solr-netd
+virsh start 011131-solr-netd
 
-virsh start 011132-solr
-virsh start 011133-solr
+virsh start 011132-solr-netd
+virsh start 011133-solr-netd
 
-virsh start 011140-solr
-virsh start 011141-solr
+virsh start 011140-solr-etcd
+virsh start 011141-solr-etcd
 
-virsh start 011142-solr
-virsh start 011143-solr
+virsh start 011142-solr-etcd
+virsh start 011143-solr-etcd
 
-virsh start 011160-solr
-virsh start 011161-solr
+virsh start 011160-solr-app
+virsh start 011161-solr-app
 
-virsh start 011162-solr
-virsh start 011163-solr
+virsh start 011162-solr-app
+virsh start 011163-solr-app
 
-virsh start 011170-solr
-virsh start 011171-solr
+virsh start 011170-solr-appd
+virsh start 011171-solr-appd
 
 virsh start 011172-solr
 virsh start 011173-solr
