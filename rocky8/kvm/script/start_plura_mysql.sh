e579bed8a7d5bbddf082c95c7a93c3a4261c6db6
Qubit Security Inc <plura@qubitsec.com>
2023-12-26 12:40:36 +0900

    Update start_plura_mysql.sh

--- rocky8/kvm/script/start_plura_mysql.sh
+++ rocky8/kvm/script/start_plura_mysql.sh
@@ -1,10 +1,22 @@
 #!/usr/bin/env bash
 
-#mysql
-virsh start 010240-mysql                   
-virsh start 010241-mysql                   
-virsh start 010242-mysql                   
-virsh start 010243-mysql                   
-virsh start 010244-mysql                   
+#mysql-maindb
+virsh start 010240-mysql
+
+#mysql-statsdb
+virsh start 010241-mysql
+
+#mysql-infradb
+virsh start 010242-mysql
+
+#mysql-maindb-s
+virsh start 010243-mysql
+
+#mysql-statsdb-s
+virsh start 010244-mysql
+
+#mysql-detectdb
 virsh start 010245-mysql
+
+#mysql-detectdb-s
 virsh start 010246-mysql
